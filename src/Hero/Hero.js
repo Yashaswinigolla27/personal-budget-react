2fd33debf7a8055d57ba29e0697f46b4f2494764
Yashaswini Golla <yashaswinigolla@Yashaswinis-MacBook-Air.local>
2023-10-01 16:18:28 -0400

    Brought PBudget code in

---
+++ src/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+            <h1 id="myH1">Personal Budget</h1>
+            <h3>We help you track your expenses...FOR FREEE!!!</h3>
+
+            {/* <h3 onclick="changeTitle()">We help you track your expenses...FOR FREEE!!!</h3> */}
+        </div>
+  );
+}
+
+export default Hero;
